1bd7c963e31fdc28165ed5f404b6cf5747fe6455
aurorg <2519652258@qq.com>
2023-04-18 20:26:20 +0800

    1

--- web_pro/springboot_mybatis_quickstart/src/test/java/com/example/SpringbootMybatisQuickstartApplicationTests.java
+++ web_pro/springboot_mybatis_quickstart/src/test/java/com/example/SpringbootMybatisQuickstartApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example;
 
+import com.example.mapper.UserMapper;
+import com.example.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest //springboot整合单元测试的注解
 class SpringbootMybatisQuickstartApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
-    void contextLoads() {
+    public void testListUser(){
+        List<User> userList =userMapper.list();
+        userList.stream().forEach(user->{
+            System.out.println(user);
+        });
     }
 
 }
