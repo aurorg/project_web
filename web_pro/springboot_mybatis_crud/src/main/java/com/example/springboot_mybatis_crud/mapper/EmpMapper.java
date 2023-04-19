df8dbeaa65f79ff9ee6feb263ff4b64b97c37c60
aurorg <2519652258@qq.com>
2023-04-19 21:29:06 +0800

    1

--- web_pro/springboot_mybatis_crud/src/main/java/com/example/springboot_mybatis_crud/mapper/EmpMapper.java
+++ web_pro/springboot_mybatis_crud/src/main/java/com/example/springboot_mybatis_crud/mapper/EmpMapper.java
@@ -1,8 +1,8 @@
 package com.example.springboot_mybatis_crud.mapper;
 
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Mapper;
+import com.example.springboot_mybatis_crud.pojo.Emp;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface EmpMapper {
@@ -11,4 +11,14 @@ public interface EmpMapper {
     @Delete("delete from mybatis.emp where id=#{id}")
     public void delete(Integer id);
 
+    //新增员工
+    @Options(useGeneratedKeys = true,keyProperty = "id")  //获取返回的主键
+    @Insert("insert into mybatis.emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time)" +
+            "            values(#{username},#{name},#{gender},#{image},#{job},#{entrydate},#{deptId},#{createTime},#{updateTime})")
+    public void insert(Emp emp);
+
+
+    //更新数据
+    @Update("update mybatis.emp set username=#{username},name=#{name},gender=#{gender},image=#{image},job=#{job},entrydate=#{entrydate},dept_id=#{deptId},update_time=#{updateTime}  where id=#{id};")
+    public void update(Emp emp);
 }
--- web_pro/springboot_mybatis_crud/src/test/java/com/example/springboot_mybatis_crud/SpringbootMybatisCrudApplicationTests.java
+++ web_pro/springboot_mybatis_crud/src/test/java/com/example/springboot_mybatis_crud/SpringbootMybatisCrudApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.springboot_mybatis_crud;
 
 import com.example.springboot_mybatis_crud.mapper.EmpMapper;
+import com.example.springboot_mybatis_crud.pojo.Emp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SpringbootMybatisCrudApplicationTests {
@@ -11,11 +15,49 @@ class SpringbootMybatisCrudApplicationTests {
     @Autowired
     private EmpMapper empMapper;
 
-
-
     @Test
     public void testDelete(){
         empMapper.delete(17);
+    }
+
+    @Test
+    public void testInsert(){
+        //构造员工对象
+        Emp emp=new Emp();
+        emp.setUsername("Tom3");
+        emp.setName("汤姆");
+        emp.setImage("1.jpg");
+        emp.setGender((short)1);
+        emp.setJob((short)1);
+        emp.setEntrydate(LocalDate.of(2000,1,1));
+        emp.setCreateTime(LocalDateTime.now());
+        emp.setUpdateTime(LocalDateTime.now());
+        emp.setDeptId(1);
+
+        //执行新增员工信息操作
+        empMapper.insert(emp);
+        System.out.println(emp.getId());
+
+    }
+
+    @Test
+    public void testUpdate(){
+        //构造员工对象
+        Emp emp=new Emp();
+        emp.setId(18);
+        emp.setUsername("Tom111");
+        emp.setName("汤姆111");
+        emp.setImage("1.jpg");
+        emp.setGender((short)1);
+        emp.setJob((short)1);
+        emp.setEntrydate(LocalDate.of(2000,1,1));
+        emp.setUpdateTime(LocalDateTime.now());
+        emp.setDeptId(1);
+
+
+        //执行更新员工操作
+        empMapper.update(emp);
+
     }
 
 
