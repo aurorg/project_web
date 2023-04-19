a44cfaa94d044acb4c72a82b1bf3f80b06d072fc
aurorg <2519652258@qq.com>
2023-04-19 16:19:52 +0800

    1

--- web_pro/springboot_mybatis_crud/src/main/java/com/example/springboot_mybatis_crud/pojo/Emp.java
+++ web_pro/springboot_mybatis_crud/src/main/java/com/example/springboot_mybatis_crud/pojo/Emp.java
@@ -11,17 +11,16 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Emp {
     private Integer id; //ID
-    private String username;
-    private String password;
-    private String name;
-    private Short gender;
-    private String image;
-    private Short job;
-    private LocalDate entrydate;
-    private Integer deptId;
-    private LocalDateTime createTime;
-    private LocalDateTime updateTime;
+    private String username; //用户名
+    private String password; //密码
+    private String name;  //姓名
+    private Short gender; //性别，1男，2女
+    private String image;//图像url
+    private Short job; //职位，1班主任，2讲师，3学生主管，4教研主管，5咨询师
+    private LocalDate entrydate; //入职日期
+    private Integer deptId; //部门id
+    private LocalDateTime createTime; //创建时间
+    private LocalDateTime updateTime; //修改时间
 }
--- web_pro/springboot_mybatis_crud/src/test/java/com/example/springboot_mybatis_crud/SpringbootMybatisCrudApplicationTests.java
+++ web_pro/springboot_mybatis_crud/src/test/java/com/example/springboot_mybatis_crud/SpringbootMybatisCrudApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.springboot_mybatis_crud;
 
+import com.example.springboot_mybatis_crud.mapper.EmpMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootMybatisCrudApplicationTests {
 
+    @Autowired
+    private EmpMapper empMapper;
+
+
+
     @Test
-    void contextLoads() {
+    public void testDelete(){
+        empMapper.delete(17);
     }
+
 
 }
