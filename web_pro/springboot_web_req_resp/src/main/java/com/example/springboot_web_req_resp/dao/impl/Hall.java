f63801c7687e12d08ac543b6ff1daaa6074ae05a
aurorg <2519652258@qq.com>
2023-05-12 16:49:09 +0800

    1

--- web_pro/springboot_web_req_resp/src/main/java/com/example/springboot_web_req_resp/dao/impl/Hall.java
+++ web_pro/springboot_web_req_resp/src/main/java/com/example/springboot_web_req_resp/dao/impl/Hall.java
@@ -1,10 +1,17 @@
 package com.example.springboot_web_req_resp.dao.impl;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Entity
-@Table(name = "hall")
+//@Table(name = "hall")
+@TableName(value="hall")
 @Getter
 @Setter
 public class Hall {
