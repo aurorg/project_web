ac0844fb4e5e02daa8703bf89b66de54eaee7ce4
aurorg <2519652258@qq.com>
2023-07-20 15:19:38 +0800

    1

---
+++ springboot2_01/src/main/java/com/example/springboot2_01/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.springboot2_01.controller;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String handle01(){
+        return "hello,Spring Boot2";
+    }
+
+}
