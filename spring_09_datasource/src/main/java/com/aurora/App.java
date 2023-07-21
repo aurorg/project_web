cd733fcbcd5a137e378469f8de5d81a5bdad366b
aurorg <2519652258@qq.com>
2023-07-21 21:37:14 +0800

    add

--- spring_09_datasource/src/main/java/com/aurora/App.java
+++ spring_09_datasource/src/main/java/com/aurora/App.java
@@ -3,9 +3,12 @@ package com.aurora;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        
+        DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+        System.out.println(dataSource);
     }
 }
