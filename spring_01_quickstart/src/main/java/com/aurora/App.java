3d40c41a400b75d4718452eb24e219f45456f9bb
aurorg <2519652258@qq.com>
2023-07-21 10:45:59 +0800

    add

--- spring_01_quickstart/src/main/java/com/aurora/App.java
+++ spring_01_quickstart/src/main/java/com/aurora/App.java
@@ -1,7 +1,11 @@
 package com.aurora;
 
+import com.aurora.service.BookService;
+import com.aurora.service.impl.BookServiceImpl;
+
 public class App {
     public static void main(String[] args){
-
+        BookService bookService = new BookServiceImpl();
+        bookService.save();
     }
 }
