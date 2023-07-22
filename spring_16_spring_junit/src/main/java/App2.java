131c7a42f7d996f5a85fcde8063f8c7c1c06805f
aurorg <2519652258@qq.com>
2023-07-22 14:29:33 +0800

    add

---
+++ spring_16_spring_junit/src/main/java/App2.java
@@ -0,0 +1,16 @@
+import com.aurora.config.SpringConfig;
+import com.aurora.domain.Account;
+import com.aurora.service.AccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App2 {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        AccountService accountService = ctx.getBean(AccountService.class);
+
+        Account ac = accountService.findById(1);
+        System.out.println(ac);
+    }
+}
---
+++ spring_16_spring_junit/src/main/java/com/aurora/service/impl/AccountServiceImpl.java
@@ -0,0 +1,35 @@
+package com.aurora.service.impl;
+
+import com.aurora.dao.AccountDao;
+import com.aurora.domain.Account;
+import com.aurora.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+    public void save(Account account) {
+        accountDao.save(account);
+    }
+
+    public void update(Account account){
+        accountDao.update(account);
+    }
+
+    public void delete(Integer id) {
+        accountDao.delete(id);
+    }
+
+    public Account findById(Integer id) {
+        return accountDao.findById(id);
+    }
+
+    public List<Account> findAll() {
+        return accountDao.findAll();
+    }
+}
