aa86f93caad604a13f446c3a5abba3c74be7e4af
aurorg <2519652258@qq.com>
2023-07-24 15:27:02 +0800

    add

---
+++ springmvc/springmvc_01_quickstart/src/main/java/com/aurora/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.aurora.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//定义表现层控制器bean
+//2.制作控制器类，等同于Servlet
+//2.1必须是一个spring管理的bean
+//2.2定义具体处理请求的方法
+//2.3设置当前方法的访问路径
+//2.4设置响应结果为json数据
+@Controller
+public class UserController {
+
+    //设置映射路径为/save，即外部访问路径
+    @RequestMapping("/save")
+    //设置当前操作返回结果为指定json数据（本质上是一个字符串信息）
+    @ResponseBody
+    public String save(){
+        System.out.println("user save ...");
+        return "{'module':'springmvc'}";
+    }
+
+//    //设置映射路径为/delete，即外部访问路径
+//    @RequestMapping("/delete")
+//    @ResponseBody
+//    public String delete(){
+//        System.out.println("user save ...");
+//        return "{'info':'springmvc'}";
+//    }
+}
