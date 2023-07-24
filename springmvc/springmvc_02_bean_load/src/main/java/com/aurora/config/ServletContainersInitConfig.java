bc49ff717f1467158be0be65721a86c117e1e89d
aurorg <2519652258@qq.com>
2023-07-24 16:34:07 +0800

    add

--- springmvc/springmvc_02_bean_load/src/main/java/com/aurora/config/ServletContainersInitConfig.java
+++ springmvc/springmvc_02_bean_load/src/main/java/com/aurora/config/ServletContainersInitConfig.java
@@ -2,18 +2,36 @@ package com.aurora.config;
 
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
-public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
-    protected WebApplicationContext createServletApplicationContext() {
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(SpringMvcConfig.class);
-        return ctx;
+public class ServletContainersInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfig.class};
     }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
+    }
+
+    @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
-    protected WebApplicationContext createRootApplicationContext() {
-        return null;
-    }
+//    protected WebApplicationContext createServletApplicationContext() {
+//        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+//        ctx.register(SpringMvcConfig.class);
+//        return ctx;
+//    }
+//
+//    protected String[] getServletMappings() {
+//        return new String[]{"/"};
+//    }
+//    protected WebApplicationContext createRootApplicationContext() {
+//        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+//        ctx.register(SpringConfig.class);
+//        return ctx;
+//    }
 }
--- springmvc/springmvc_02_bean_load/src/main/java/com/aurora/config/SpringMvcConfig.java
+++ springmvc/springmvc_02_bean_load/src/main/java/com/aurora/config/SpringMvcConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
-//@Configuration
+@Configuration
 @ComponentScan("com.aurora.controller")
 public class SpringMvcConfig {
 }
