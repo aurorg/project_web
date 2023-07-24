01bad4e160bccb7ea3be3aa3cb3b7c2e8c21c126
aurorg <2519652258@qq.com>
2023-07-24 11:21:09 +0800

    fix

---
+++ spring_24_case_transfer/src/main/java/com/aurora/service/impl/AccountServiceImpl.java
@@ -0,0 +1,22 @@
+package com.aurora.service.impl;
+
+import com.aurora.dao.AccountDao;
+import com.aurora.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+
+    public void transfer(String out,String in ,Double money) {
+        accountDao.outMoney(out,money);
+        int i = 1/0;
+        accountDao.inMoney(in,money);
+    }
+
+}
