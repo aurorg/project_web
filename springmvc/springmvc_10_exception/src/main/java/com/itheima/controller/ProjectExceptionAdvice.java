58d965be50f0c813cf4f1bd4e399957cc031020a
aurorg <2519652258@qq.com>
2023-07-28 15:01:59 +0800

    fix

--- springmvc/springmvc_10_exception/src/main/java/com/itheima/controller/ProjectExceptionAdvice.java
+++ springmvc/springmvc_10_exception/src/main/java/com/itheima/controller/ProjectExceptionAdvice.java
@@ -1,12 +1,32 @@
 package com.itheima.controller;
 
+import com.itheima.exception.BusinessException;
+import com.itheima.exception.SystemException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ProjectExceptionAdvice {
+    //处理其他异常
     @ExceptionHandler(Exception.class) //定义处理哪一种异常
     public Result doException(Exception ex){
-       return new Result(000,null,"异常！！！！");
+       return new Result(Code.SYSTEM_UNKNOW_ERR,null,"系统繁忙，稍后再试");
+    }
+
+
+    //处理系统异常
+    @ExceptionHandler(SystemException.class)
+    public Result doSystemException(SystemException ex){
+        //记录日志
+        //发送消息给运维
+        //发送邮件给开发人员
+        return new Result(ex.getCode(),null,ex.getMessage());
+    }
+
+    //处理业务异常
+    @ExceptionHandler(BusinessException.class)
+    public Result doBusinessExceptionException(BusinessException ex){
+
+        return new Result(ex.getCode(),null,ex.getMessage());
     }
 }
--- springmvc/springmvc_10_exception/src/main/java/com/itheima/service/impl/BookServiceImpl.java
+++ springmvc/springmvc_10_exception/src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -1,7 +1,9 @@
 package com.itheima.service.impl;
 
+import com.itheima.controller.Code;
 import com.itheima.dao.BookDao;
 import com.itheima.domain.Book;
+import com.itheima.exception.SystemException;
 import com.itheima.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,15 @@ public class BookServiceImpl implements BookService {
     private BookDao bookDao;
 
     public boolean save(Book book) {
+
+        //将可能出现的异常进行包装，转换成自定义异常
+        try{
+            int i=1/0;
+
+        }catch(Exception e){
+            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时",e);
+        }
+
         bookDao.save(book);
         return true;
     }
