fb42011ce84ca41462241558dffef2558770e1ea
aurorg <2519652258@qq.com>
2023-07-29 09:32:34 +0800

    add

---
+++ MyBatisPlus/mybatisplus_01_quickstart/src/main/java/com/example/mybatisplus_01_quickstart/domain/User.java
@@ -0,0 +1,50 @@
+package com.example.mybatisplus_01_quickstart.domain;
+
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String tel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
+}
--- MyBatisPlus/mybatisplus_01_quickstart/src/test/java/com/example/mybatisplus_01_quickstart/Mybatisplus01QuickstartApplicationTests.java
+++ MyBatisPlus/mybatisplus_01_quickstart/src/test/java/com/example/mybatisplus_01_quickstart/Mybatisplus01QuickstartApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.mybatisplus_01_quickstart;
 
+import com.example.mybatisplus_01_quickstart.dao.UserDao;
+import com.example.mybatisplus_01_quickstart.domain.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class Mybatisplus01QuickstartApplicationTests {
 
+    @Autowired
+    private UserDao userDao;
     @Test
-    void contextLoads() {
+    void testGetAll() {
+        List<User> userList=userDao.selectList(null);
+        System.out.println(userList);
     }
 
 }
