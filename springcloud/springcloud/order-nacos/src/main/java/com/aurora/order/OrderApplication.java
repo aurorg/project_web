3da18fe48633abbc04dca45f9abe774d15902c0e
aurorg <2519652258@qq.com>
2023-08-01 15:44:22 +0800

    add

--- springcloud/springcloud/order-nacos/src/main/java/com/aurora/order/OrderApplication.java
+++ springcloud/springcloud/order-nacos/src/main/java/com/aurora/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.aurora.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ public class OrderApplication {
     }
 
     @Bean
+    @LoadBalanced   //负载均衡器
     public RestTemplate restTemplate(RestTemplateBuilder builder){
         RestTemplate restTemplate =builder.build();
         return restTemplate;
