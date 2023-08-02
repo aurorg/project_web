1a30be9d0d1fe62b916e2fb804538b225e0516ca
aurorg <2519652258@qq.com>
2023-08-02 19:48:43 +0800

    1

--- springcloud/springcloud/config-nacos/src/main/java/com/aurora/config/ConfigApplication.java
+++ springcloud/springcloud/config-nacos/src/main/java/com/aurora/config/ConfigApplication.java
@@ -3,11 +3,15 @@ package com.aurora.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class,args);
+        ConfigurableApplicationContext applicationContext=SpringApplication.run(ConfigApplication.class,args);
+        String userName =applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name:" + userName +"; age: " + userAge);
 
     }
 
