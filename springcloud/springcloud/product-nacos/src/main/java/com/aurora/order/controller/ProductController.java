cf080a74bb589c36c5e5529527a302de389ca3ce
aurorg <2519652258@qq.com>
2023-08-02 15:39:38 +0800

    add

---
+++ springcloud/springcloud/product-nacos/src/main/java/com/aurora/order/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.aurora.order.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/{id}")
+    public String get(@PathVariable Integer id) throws InterruptedException {
+        Thread.sleep(4000);
+        System.out.println("查询商品"+id);
+        return "查询商品"+id+":"+port;
+    }
+
+}
