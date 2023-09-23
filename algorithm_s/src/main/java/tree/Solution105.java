f4ec1474f4e2d936c01900c1defff308a5161c50
aurorg <2519652258@qq.com>
2023-09-23 14:27:12 +0800

    add

---
+++ algorithm_s/src/main/java/tree/Solution105.java
@@ -0,0 +1,104 @@
+package tree;
+
+import java.util.*;
+
+class Solution {
+    // 存储 inorder 中值到索引的映射
+    HashMap<Integer, Integer> valToIndex = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            valToIndex.put(inorder[i], i);
+        }
+        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    /*
+       定义：前序遍历数组为 preorder[preStart..preEnd]，
+       中序遍历数组为 inorder[inStart..inEnd]，
+       构造这个二叉树并返回该二叉树的根节点
+    */
+    TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd) {
+            return null;
+        }
+
+        // root 节点对应的值就是前序遍历数组的第一个元素
+        int rootVal = preorder[preStart];
+        // rootVal 在中序遍历数组中的索引
+        int index = valToIndex.get(rootVal);
+
+        int leftSize = index - inStart;
+
+        // 先构造出当前根节点
+        TreeNode root = new TreeNode(rootVal);
+
+        // 递归构造左右子树
+        root.left = build(preorder, preStart + 1, preStart + leftSize, inorder, inStart, index - 1);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd, inorder, index + 1, inEnd);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+
+        TreeNode root = solution.buildTree(preorder, inorder);
+
+        // 按指定格式输出二叉树
+        String output = levelOrderToString(root);
+        System.out.println(output);
+    }
+
+    // 层序遍历并按指定格式输出二叉树
+    public static String levelOrderToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                sb.append(node.val);
+                sb.append(",");
+                queue.offer(node.left);
+                queue.offer(node.right);
+            } else {
+                sb.append("null,");
+            }
+        }
+
+        // 移除最后一个多余的逗号
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    // Definition for a binary tree node
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
