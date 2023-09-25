e0712362f65e5f19263521198bf598d371f46f68
aurorg <2519652258@qq.com>
2023-09-25 21:35:49 +0800

    1

--- algorithm_s/src/main/java/treetest/solution105.java
+++ algorithm_s/src/main/java/treetest/solution105.java
@@ -9,15 +9,11 @@ public class solution105 {
         int val;
         TreeNode left;
         TreeNode right;
-
-        TreeNode(){
-
-        }
+        TreeNode(){}
 
         TreeNode(int val){
             this.val=val;
         }
-
         TreeNode(int val,TreeNode left,TreeNode right){
             this.val=val;
             this.left=left;
@@ -25,37 +21,35 @@ public class solution105 {
         }
     }
 
-
-    HashMap<Integer,Integer> valToIndex = new HashMap<>();
+    //存储inorder中的值到索引的映射
+    HashMap<Integer,Integer> valToIndex= new HashMap<>();
 
     public TreeNode buildTree(int[] preorder,int[] inorder){
         for(int i=0;i<inorder.length;i++){
             valToIndex.put(inorder[i],i);
         }
-        return build(preorder,0,preorder.length,
+        return build(preorder,0,preorder.length-1,
                 inorder,0,inorder.length-1);
-
     }
 
     TreeNode build(int[] preorder,int preStart,int preEnd,
-                   int[] inorder,int inStart,int inEnd){
+                   int[] inorder, int inStart,int inEnd){
         if(preStart > preEnd){
             return null;
         }
 
-        int rootVal =preorder[preStart];
-
+        int rootVal = preorder[preStart];
         int index = valToIndex.get(rootVal);
 
-        int leftSize =index - inStart;
+        int leftSize = index -inStart;
 
         TreeNode root = new TreeNode(rootVal);
-
         root.left = build(preorder,preStart+1,preStart +leftSize,
-                inorder,inStart,index-1);
-        root.right = build(preorder,preStart+leftSize+1,preEnd,
+                inorder,inStart,index - 1);
+        root.right=build(preorder,preStart+leftSize+1,preEnd,
                 inorder,index+1,inEnd);
         return root;
+
     }
 
 
